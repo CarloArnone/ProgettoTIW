2ee08be70c28477a104d9b2fb45c118696e9d715
CarloArnone <carlo.arnone@mail.polimi.it>
2023-05-05 13:49:07 +0200

    upload project

---
+++ src/main/java/it/polimi/tiw/asteHTML/Controllers/Vendo.java
@@ -0,0 +1,29 @@
+package it.polimi.tiw.asteHTML.Controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "PaginaVendo", value = "/vendo")
+public class Vendo extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Vendo";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/it/polimi/tiw/asteHTML/DAO/AstaDao.java
@@ -0,0 +1,4 @@
+package it.polimi.tiw.asteHTML.DAO;
+
+public class AstaDao {
+}
